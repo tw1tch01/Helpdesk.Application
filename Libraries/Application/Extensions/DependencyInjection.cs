de9bfb800a964421a6b49f57bb93bd0d8c628400
Robert <robbiebreedt@yahoo.com>
2020-08-10 06:35:35 +0200

    dependencyinjection

--- Libraries/Application/Extensions/DependencyInjection.cs
+++ Libraries/Application/Extensions/DependencyInjection.cs
@@ -1,18 +1,17 @@
-﻿using System.Reflection;
+﻿using Helpdesk.Services.Common;
 using Helpdesk.Services.Extensions;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Helpdesk.Application.Extensions
 {
-    public static class IServiceCollectionExtensions
+    public static class DependencyInjection
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddServices();
 
-            services.AddMediatR(Assembly.GetExecutingAssembly());
-
+            services.AddMediatR(typeof(DependencyInjection).Assembly);
             return services;
         }
     }
